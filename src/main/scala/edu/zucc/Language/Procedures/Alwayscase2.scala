f38dcffae38bf2ce1addd04ea51cbd2e0bbb3cd3
fanliyong007 <fanliyong007@hotmail.com>
2023-01-29 23:57:45 +0800

    will complete proceduer again....

---
+++ src/main/scala/edu/zucc/Language/Procedures/Alwayscase2.scala
@@ -0,0 +1,23 @@
+package edu.zucc.Language.Procedures
+
+import chisel3.stage.ChiselStage
+import chisel3.util.{is, _}
+import chisel3._
+class Alwayscase2  extends Module {
+  val in=IO(Input(UInt(4.W)))
+  val pos=IO(Output(UInt(2.W)))
+
+  val pos_reg=Reg(UInt(2.W))
+  pos:=pos_reg
+  pos_reg:=PriorityEncoder(in)
+  //PriorityEncoder 有大问题会生成错误default情况
+//  switch(in){
+//    is("b???1".U) {tmp_pos := "b00".U}
+//    is ("b??10".U) {tmp_pos := "b01".U}
+//    is ("b?100".U) {tmp_pos := "b10".U}
+//    is ("b1000".U) {tmp_pos := "b11".U}
+//  }
+}
+object Alwayscase2 extends App{
+  (new ChiselStage).emitVerilog(new Alwayscase2,Array("-td","vout"))
+}
---
+++ src/main/scala/edu/zucc/Language/Procedures/Alwaysif2.scala
@@ -0,0 +1,33 @@
+package edu.zucc.Language.Procedures
+
+import chisel3.stage.ChiselStage
+import chisel3.util._
+import chisel3._
+import chisel3.stage.ChiselStage
+
+class Alwaysif2 extends Module {
+  val cpu_overheated=IO(Input(Bool()))
+  val shut_off_computer=IO(Output(UInt(1.W)))
+  val arrived=IO(Input(Bool()))
+  val gas_tank_empty=IO(Input(UInt(1.W)))
+  val keep_driving=IO(Output(UInt(1.W)))
+
+  val t_shut_off_computer=Reg(UInt(1.W))
+  val t_keep_driving=Reg(UInt(1.W))
+
+  shut_off_computer:=t_shut_off_computer
+  keep_driving:=t_keep_driving
+  when(cpu_overheated){
+    t_shut_off_computer:=1.U
+  }.otherwise{
+    t_shut_off_computer:=0.U
+  }
+  when((!arrived)&&(!gas_tank_empty)) {
+    t_keep_driving := 1.U
+  }.otherwise {
+    t_keep_driving := 0.U
+  }
+}
+object Alwaysif2 extends App{
+  (new ChiselStage).emitVerilog(new Alwaysif2,Array("-td","vout"))
+}
