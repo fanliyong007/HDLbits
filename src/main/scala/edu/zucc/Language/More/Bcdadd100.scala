c7335c5d14f156605c39728c43f3133de3cc3721
fanliyong007 <fanliyong007@hotmail.com>
2023-04-30 13:48:46 +0800

    use Chisel in Verilog Language ok

--- src/main/scala/edu/zucc/Language/More/Bcdadd100.scala
+++ src/main/scala/edu/zucc/Language/More/Bcdadd100.scala
@@ -2,17 +2,31 @@ package edu.zucc.Language.More
 import chisel3.stage.ChiselStage
 import chisel3.util._
 import chisel3._
-import chisel3.internal.firrtl.Width
+
 class Bcdadd100 extends Module {
-  val a = IO(Input(UInt(399.W)))
-  val b = IO(Input(UInt(399.W)))
-  val cin = IO(Input(UInt(1.W)))
-  val cout = IO(Output(UInt(1.W)))
-  val sum = IO(Output(UInt(399.W)))
+  val io = IO(new Bundle {
+    val a = Input(UInt(400.W))
+    val b = Input(UInt(400.W))
+    val cin = Input(Bool())
+    val cout = Output(Bool())
+    val sum = Output(UInt(400.W))
+  })
 
+  val u_bcd_fadd = Array.fill(100)(Module(new bcd_fadd()).io)
+  val bcd_sum = Wire(Vec(100, UInt(4.W)))
+  val bcd_carry = Wire(Vec(101, Bool()))
 
+  bcd_carry(0):=io.cin
+  for(i <- 0 until 100){
+    u_bcd_fadd(i).a := Cat(io.a(i * 4 + 3), io.a(i * 4 + 2), io.a(i * 4 + 1), io.a(i * 4))
+    u_bcd_fadd(i).b := Cat(io.b(i * 4 + 3), io.b(i * 4 + 2), io.b(i * 4 + 1), io.b(i * 4))
+    u_bcd_fadd(i).cin:=bcd_carry(i)
+    bcd_carry(i+1):=u_bcd_fadd(i).cout
+    bcd_sum(i):=u_bcd_fadd(i).sum
+  }
+  io.cout:=bcd_carry(100)
+  io.sum:=bcd_sum.asUInt
 }
-
 
 object Bcdadd100 extends App{
   (new ChiselStage).emitVerilog(new Bcdadd100,Array("-td","vout"))
