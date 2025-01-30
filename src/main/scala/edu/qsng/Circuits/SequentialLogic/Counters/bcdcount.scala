58224653e1fe57cd4e90c29ce386dddeccf0dae3
fanliyong007 <fanliyong007@hotmail.com>
2025-01-30 15:12:43 +0800

    remember == and === isn's same

--- src/main/scala/edu/qsng/Circuits/SequentialLogic/Counters/bcdcount.scala
+++ src/main/scala/edu/qsng/Circuits/SequentialLogic/Counters/bcdcount.scala
@@ -2,10 +2,11 @@ package edu.qsng.Circuits.SequentialLogic.Counters
 import chisel3._
 
 class bcdcount extends BlackBox {
-  val io=IO(new Bundle {
-    val clk = Input(Clock())
+  val io = IO(new Bundle{
+    val clk =Input(Clock())
     val reset = Input(Bool())
     val enable = Input(Bool())
     val Q= Output(UInt(4.W))
   })
+
 }
--- src/main/scala/edu/qsng/Circuits/SequentialLogic/Counters/ece241_2014_q7b.scala
+++ src/main/scala/edu/qsng/Circuits/SequentialLogic/Counters/ece241_2014_q7b.scala
@@ -9,21 +9,26 @@ class ece241_2014_q7b extends Module with ImplicitClock {
   val OneHertz = IO(Output(Bool()))
   val c_enable = IO(Output(UInt(3.W)))
   override protected def implicitClock = clk
-  val Q = Wire(UInt(12.W))
-  val bc = Array.fill(3)(Module(new bcdcount()))
-  bc(0).io.clk := clk
-  bc(0).io.reset := reset
-  bc(0).io.enable := c_enable(0)
-  bc(1).io.clk := clk
-  bc(1).io.reset := reset
-  bc(1).io.enable := c_enable(1)
-  bc(2).io.clk := clk
-  bc(2).io.reset := reset
-  bc(2).io.enable := c_enable(2)
-  Q := Cat(bc(0).io.Q,bc(1).io.Q,bc(2).io.Q)
-  c_enable := Cat(1.U,Q(3,0)===9.U,Q(7,0)===153.U)
-  OneHertz := Q(11,0)===2457.U
+  val counter = Array.fill(3)(Module(new bcdcount()))
+  val q = Array.fill(3)(Wire(UInt(4.W)))
 
+  counter(0).io.clk := clk
+  counter(0).io.reset := reset
+  counter(0).io.enable := c_enable(0)
+  q(0) := counter(0).io.Q
+
+  counter(1).io.clk := clk
+  counter(1).io.reset := reset
+  counter(1).io.enable := c_enable(1)
+  q(1) := counter(1).io.Q
+
+  counter(2).io.clk := clk
+  counter(2).io.reset := reset
+  counter(2).io.enable := c_enable(2)
+  q(2) := counter(2).io.Q
+
+  c_enable := Cat((q(1)===9.U)&&(q(0)===9.U),q(0)===9.U, 1.U)//注意使用===而非===
+  OneHertz := (q(2)===9.U)&&(q(1)===9.U)&&(q(0)===9.U)
 }
 
 object ece241_2014_q7b extends App{
