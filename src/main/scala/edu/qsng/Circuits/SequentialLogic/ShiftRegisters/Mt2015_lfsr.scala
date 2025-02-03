bf9bb833728c10eec495731e5306eef744d2de17
fanliyong007 <fanliyong007@hotmail.com>
2025-02-03 23:19:40 +0800

    add 1

--- src/main/scala/edu/qsng/Circuits/SequentialLogic/ShiftRegisters/Mt2015_lfsr.scala
+++ src/main/scala/edu/qsng/Circuits/SequentialLogic/ShiftRegisters/Mt2015_lfsr.scala
@@ -8,7 +8,21 @@ class Mt2015_lfsr extends Module with ImplicitClock {
   val SW = IO(Input(UInt(3.W)))
   val KEY = IO(Input(UInt(2.W)))
   val LEDR = IO(Output(UInt(3.W)))
+  override protected def implicitClock = clk
+  val clk=Wire(Clock())
+  val L  =Wire(UInt(1.W))
+  val R  =Wire(UInt(3.W))
+  val Q  =Reg(UInt(3.W))
+  clk := KEY(0).asClock;
+  L   := KEY(1);
+  R   := SW;
 
+  Q := Cat(
+    Mux(L.asBool, R(2) ,(Q(1) ^ Q(2))),
+    Mux(L.asBool,R(1) , Q(0)),
+    Mux(L.asBool,R(0) , Q(2)))
+
+  LEDR := Q
 
 }
 object Mt2015_lfsr extends App{
