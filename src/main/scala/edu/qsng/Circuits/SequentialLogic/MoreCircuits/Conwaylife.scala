bae0a3d988f36945360b853a7c3527548b559716
fanliyong007 <fanliyong007@hotmail.com>
2025-02-08 22:37:11 +0800

    I will built a new hardware for racer so the project maybe stop

--- src/main/scala/edu/qsng/Circuits/SequentialLogic/MoreCircuits/Conwaylife.scala
+++ src/main/scala/edu/qsng/Circuits/SequentialLogic/MoreCircuits/Conwaylife.scala
@@ -1,16 +1,55 @@
 package edu.qsng.Circuits.SequentialLogic.MoreCircuits
 import chisel3._
-import chisel3.util.Cat
+import chisel3.util.{Cat, MuxCase}
 import circt.stage.ChiselStage
 
 class Conwaylife extends Module with ImplicitClock {
 
   val clk = IO(Input(Clock()))
   val load = IO(Input(Bool()))
-  val data= IO(Input(UInt(512.W)))
-  val q = IO(Output(UInt(512.W)))
+  val data= IO(Input(UInt(256.W)))
+  val q = IO(Output(UInt(256.W)))
   override protected def implicitClock = clk
+  val nextQ = Wire(Vec(256, UInt(1.W)))
+  for (i <- 0 until 256) {
+    val currentQ = q
+    val sum = {
+      if (i == 0) {
+        currentQ(1) + currentQ(16) + currentQ(17) + currentQ(240) + currentQ(241) + currentQ(15) + currentQ(31) + currentQ(255)
+      } else if (i == 15) {
+        currentQ(14) + currentQ(16) + currentQ(0) + currentQ(240) + currentQ(254) + currentQ(30) + currentQ(31) + currentQ(255)
+      } else if (i == 240) {
+        currentQ(0) + currentQ(15) + currentQ(239) + currentQ(241) + currentQ(1) + currentQ(224) + currentQ(225) + currentQ(255)
+      } else if (i == 255) {
+        currentQ(0) + currentQ(15) + currentQ(14) + currentQ(224) + currentQ(238) + currentQ(240) + currentQ(239) + currentQ(254)
+      } else if (i > 0 && i < 15) {
+        currentQ(i-1) + currentQ(i+1) + currentQ(i+15) + currentQ(i+16) + currentQ(i+17) + currentQ(i+239) + currentQ(i+240) + currentQ(i+241)
+      } else if (i % 16 == 0) {
+        currentQ(i-1) + currentQ(i+1) + currentQ(i+15) + currentQ(i+16) + currentQ(i+17) + currentQ(i-16) + currentQ(i-15) + currentQ(i+31)
+      } else if (i % 16 == 15) {
+        currentQ(i-1) + currentQ(i+1) + currentQ(i+15) + currentQ(i+16) + currentQ(i-17) + currentQ(i-16) + currentQ(i-15) + currentQ(i-31)
+      } else if (i > 240 && i < 255) {
+        currentQ(i-1) + currentQ(i+1) + currentQ(i-17) + currentQ(i-16) + currentQ(i-15) + currentQ(i-239) + currentQ(i-240) + currentQ(i-241)
+      } else {
+        currentQ(i-1) + currentQ(i+1) + currentQ(i-17) + currentQ(i-16) + currentQ(i-15) + currentQ(i+15) + currentQ(i+16) + currentQ(i+17)
+      }
+    }
 
+    val newBit = Wire(UInt(1.W))
+    when (sum === 0.U || sum === 1.U || sum >= 4.U) {
+      newBit := 0.U
+    }.elsewhen (sum === 3.U) {
+      newBit := 1.U
+    }.otherwise {
+      newBit := currentQ(i)
+    }
+    nextQ(i) := newBit
+  }
+  when(load) {
+    q := data
+  }.otherwise {
+    q := nextQ.asUInt
+  }
 }
 object Conwaylife extends App{
   ChiselStage.emitSystemVerilogFile(
--- src/main/scala/edu/qsng/Circuits/SequentialLogic/MoreCircuits/Rule110.scala
+++ src/main/scala/edu/qsng/Circuits/SequentialLogic/MoreCircuits/Rule110.scala
@@ -14,16 +14,12 @@ class Rule110 extends Module with ImplicitClock {
   when(load){
     regQ:=data
   }.otherwise {
-    val shifted = Cat(regQ(510, 0), 0.U)
-    regQ := ((regQ ^ shifted) & regQ(511, 1)) | ((regQ | shifted) & ( ~ regQ(511, 1)))
+    val L = Cat(0.U,regQ(511,1))
+    val R = Cat(regQ(510,0),0.U)
+    regQ := ((~L) & (regQ | R))|(L & (~regQ) & R) | (L & regQ & (~R))
   }
   q:=regQ
-}//(((q[511:0] ^ {q[510:0], 1'b0}) & q[511:1]) | ((q[511:0] | {q[510:0], 1'b0})
-//{1'h0,(regQ[510:0] ^ {regQ[509:0], 1'h0}) & regQ[511:1] | (regQ[510:0] | {regQ[509:0], 1'h0})
-//73c7461f09a33e269beef89be26f8e26e9be26f89bef8ee3877c0bc4df137c4d0000000000000000000000000000000000000000000000000000000000000000
-//7 0111   5 0101  d  1101
-//564dce311be7626fbe3b89be26f89a6fbbe26f89be389ba68dc41e4df137c4df0000000000000000000000000000000000000000000000000000000000000000
-//d64dce311be7626fbe3b89be26f89a6fbbe26f89be389ba68dc41e4df137c4df0000000000000000000000000000000000000000000000000000000000000000
+}
 object Rule110 extends App{
   ChiselStage.emitSystemVerilogFile(
     gen = new Rule110,
